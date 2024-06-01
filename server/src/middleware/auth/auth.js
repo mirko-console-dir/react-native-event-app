afe4486b3c94e27cdcfb2597f1ff69282e43a207
mirko-console-dir <cdmirko.saponaro@gmail.com>
2024-06-02 04:31:54 +0900

    redis server side for memos, add to the user sign in + sign up + verify token the fullname field

--- server/src/middleware/auth/auth.js
+++ server/src/middleware/auth/auth.js
@@ -10,17 +10,20 @@ Making authenticated requests: The client sends the access token in the Authoriz
 Access token expires: Access tokens are short-lived. When the access token expires, the client uses the refresh token to request a new access token.
 
 Refreshing the token: If the refresh token is valid, the server issues a new access token (and possibly a new refresh token), and the client can continue to make authenticated requests with the new access token. */
+
+// REMEMBER DON'T MAKE AS A PROMISE WITH ASYNC
 const authenticateUser = (req) => {
 
     const authHeader = req.headers.authorization
     
     if (authHeader) {
         const token = authHeader.split('Bearer ')[1];
-        console.log('token', token);
+        //console.log('token', token);
         if (token) {
             try {
                 const user = jwt.verify(token, ACCESS_TOKEN_SECRET);
-                console.log("User authenticated:", user); // log the authenticated user
+                //console.log("User authenticated:", user); // log the authenticated user   
+              
                 return user;
             } catch (error) {
                 //console.error("Token verification error:", error);
